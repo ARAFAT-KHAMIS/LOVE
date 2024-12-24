c5a6d616338e42f3424ba1b7fe8b0d2f863d33a0
ARAFAT-KHAMIS <wearegiant06@gmail.com>
2024-12-24 13:03:25 +0300

    Initial commit

---
+++ PrintDouble.java
@@ -0,0 +1,6 @@
+public class PrintDouble {
+    public static void main(String[] args) {
+        double myDouble = 3.14159; // Example double number
+        System.out.println(myDouble);
+    }
+}
